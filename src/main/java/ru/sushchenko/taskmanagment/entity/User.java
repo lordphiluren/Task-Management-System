a63b401c430a048d430238b58bc3ce2ca7a87427
Artyom Sushchenko <artoymsushchenko@gmail.com>
2023-12-09 19:52:37 +0300

    Initial commit

---
+++ src/main/java/ru/sushchenko/taskmanagment/entity/User.java
@@ -0,0 +1,2 @@
+package ru.sushchenko.taskmanagment.entity;public class User {
+}
---
+++ src/test/java/ru/sushchenko/taskmanagment/TaskmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sushchenko.taskmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
